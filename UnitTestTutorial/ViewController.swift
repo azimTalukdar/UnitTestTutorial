3bdc82d8bd8c13a93a075bd5e16013f36ef85eab
Apple <Apple@apples-imac.local>
2018-07-09 22:01:11 +0530

    Initial Commit

---
+++ UnitTestTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTestTutorial
+//
+//  Created by Apple on 09/07/18.
+//  Copyright © 2018 AzimTalukdar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
